432496b94be89ff9384edc06a9cc3051652370b1
geg <german.grekov.01@bk.ru>
2025-10-20 18:08:03 +0300

    17 работа

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -3,18 +3,22 @@ from app.storage import DataStorage
 
 class TestDataStorage(unittest.TestCase):
     def setUp(self):
+        """Подготавливает тестовую среду перед каждым тестом"""
         self.storage = DataStorage()
 
     def test_add_and_get_item(self):
+        """Проверяет добавление и получение элемента"""
         self.storage.add_item("user", "Герман")
         self.assertEqual(self.storage.get_item("user"), "Герман")
 
     def test_remove_item(self):
+        """Проверяет удаление элемента"""
         self.storage.add_item("temp", 123)
         self.storage.remove_item("temp")
         self.assertIsNone(self.storage.get_item("temp"))
 
     def test_count_items(self):
+        """Проверяет количество элементов"""
         self.storage.add_item("a", 1)
         self.storage.add_item("b", 2)
         self.assertEqual(self.storage.count(), 2)
